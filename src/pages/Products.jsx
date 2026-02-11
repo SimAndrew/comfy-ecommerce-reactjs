571bf7b827e7e69d6950f3585420c43094d6b951
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2026-02-11 18:48:14 +0200

    feat: enhance Filters and Products components with parameter handling for search, category, company, shipping, order, and price

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,12 +4,15 @@ import { customFetch } from '../utils';
 const url = '/products';
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const loader = async () => {
-	const response = await customFetch(url);
-
+export const loader = async ({ request }) => {
+	const params = Object.fromEntries([
+		...new URL(request.url).searchParams.entries(),
+	]);
+	const response = await customFetch(url, { params });
 	const products = response.data.data;
 	const meta = response.data.meta;
-	return { products, meta };
+
+	return { products, meta, params };
 };
 
 const Products = () => {
